049c565d7bf1e39eec6239e5080160b2721dbe75
Alexis Bellido <alexis@ventanazul.com>
2020-11-05 22:48:08 -0500

    ndarrays

---
+++ nd-array-1.py
@@ -0,0 +1,15 @@
+# import pandas
+import numpy as np
+
+
+data = np.random.randn(2, 3)
+
+print(data)
+print('--------')
+
+print(data * 10)
+print('--------')
+
+print(data + data)
+print('--------')
+
